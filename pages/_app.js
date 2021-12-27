69450f045b5f600b114aa9c0608f3e37f7d2f042
PatThompson88 <patrick.thompson688@gmail.com>
2021-12-27 00:31:01 -0500

    Header created.  Home page under construction.
    	Header based on mdb-react-ui-kit
    	Heading image and titles custom bootstrap 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '../styles/globals.css';
+import 'scss/custom-styles.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
